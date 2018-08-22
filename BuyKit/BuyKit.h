268eabd55808b554cba1603eeac63110a9b901e3
Andrew De Ponte <cyphactor@gmail.com>
2018-08-21 18:06:43 -0700

    Initial commit
    This is a commit of the initial generated skeleton that Xcode produced
    for an iOS Cocoa Touch framework.

---
+++ BuyKit/BuyKit.h
@@ -0,0 +1,19 @@
+//
+//  BuyKit.h
+//  BuyKit
+//
+//  Created by Andrew De Ponte on 8/21/18.
+//  Copyright © 2018 UpTech Works, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuyKit.
+FOUNDATION_EXPORT double BuyKitVersionNumber;
+
+//! Project version string for BuyKit.
+FOUNDATION_EXPORT const unsigned char BuyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuyKit/PublicHeader.h>
+
+
