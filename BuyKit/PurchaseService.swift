7d5d28e06e96f965cab553b39ec6933b484f4db8
Ryan Hedges <ryanhedges@gmail.com>
2018-10-25 16:28:16 -0700

    Update name of observer function

--- BuyKit/PurchaseService.swift
+++ BuyKit/PurchaseService.swift
@@ -1,7 +1,7 @@
 import StoreKit
 
 public protocol PurchaseServiceObserver {
-    func finishedPurchase(transactionId: String, productId: String)
+    func purchaseComplete(transactionId: String, productId: String)
 }
 
 open class PurchaseService: NSObject {
@@ -77,7 +77,7 @@ extension PurchaseService: SKPaymentTransactionObserver {
     private func complete(transaction: SKPaymentTransaction) {
         ReceiptRepository.shared.recordPurchase(skProductId: transaction.payment.productIdentifier)
         // StoreKit transaction will have transactionIdentifier when it has been purchased
-        observers.forEach({ $0.value?.finishedPurchase(transactionId: transaction.transactionIdentifier!, productId: transaction.payment.productIdentifier) })
+        observers.forEach({ $0.value?.purchaseComplete(transactionId: transaction.transactionIdentifier!, productId: transaction.payment.productIdentifier) })
         SKPaymentQueue.default().finishTransaction(transaction)
     }
 
